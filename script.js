397ea41a355961c4f5051200ddaa262af68597ed
Medhavimangal43 <medhavimangal43@gmail.com>
2024-10-01 22:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+
+for(let i = 0; i < squares.length; i++){
+	squares[i].addEventListener("mouseover", colorchange);
+	squares[i].addEventListener("mouseout", colorrevert);
+}
+
+function colorchange(e) {
+	for(let i = 0; i < squares.length; i++){
+		if(squares[i] !== e.target){
+			squares[i].style.backgroundColor = "#6F4E37";
+		}
+	}
+}
+
+function colorrevert(e) {
+	for(let i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = "#E6E6FA";
+	}
+}
